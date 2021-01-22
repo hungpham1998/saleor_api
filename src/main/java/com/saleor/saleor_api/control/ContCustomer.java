ffbeb0ad04ec34e1f458635493efa6a37c17dd49
Vũ Quang Long <quanglong12g@gmail.com>
2021-01-22 12:38:53 +0700

    fix

--- src/main/java/com/saleor/saleor_api/control/ContCustomer.java
+++ src/main/java/com/saleor/saleor_api/control/ContCustomer.java
@@ -16,6 +16,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 @Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("api/customer")
 public class ContCustomer {
     @Autowired
     SerCustomer serCustomer;
